6444e79750b871f521315747c5b4bdcaef9d18e4
Ryan <Ryan@DESKTOP-9JD2JB2>
2023-07-12 15:51:39 -0700

    Add project files.

---
+++ CustomerRelationsManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerRelationsManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
