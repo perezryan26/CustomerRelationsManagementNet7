491d06c6f0c889ddb4f7a23d00b187e17625b9f8
Ryan Perez <perezryan26@gmail.com>
2023-07-19 21:01:11 -0700

    Newest Working Version

--- CustomerRelationsManagement.Web/Models/ErrorViewModel.cs
+++ CustomerRelationsManagement.Web/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace CustomerRelationsManagement.Web.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
