f7dc5d84c0a12581f1a832e5c1467d3418cba704
Ryan Perez <perezryan26@gmail.com>
2023-09-20 18:03:25 -0700

    Newest Version

---
+++ CustomerRelationsManagement.Data/Paycheck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationsManagement.Data
+{
+    public class Paycheck : BaseEntity
+    {
+        public int EmployeeId { get; set; }
+        public int Hours { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ CustomerRelationsManagement.Data/Shift.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerRelationsManagement.Data
+{
+    public class Shift : BaseEntity
+    {
+        public int EmployeeId { get; set; }
+        public int TimeOfShift { get; set; }
+    }
+}
